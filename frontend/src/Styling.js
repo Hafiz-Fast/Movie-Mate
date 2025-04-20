b14ee6b82a8682e50c80dcab3f110dade69cea17
Abdullah-Ejaz0 <l230537@lhr.nu.edu.pk>
2025-04-20 23:36:34 +0500

    Better Modularity and Future Setup

--- frontend/src/Styling.js
+++ frontend/src/Styling.js
@@ -3,6 +3,8 @@ import { useEffect } from 'react';
 
 import login from './js/Login';
 import home from './js/Home';
+import './css/home.css';
+import './css/login.css';
 
 const RouteLogger = () => {
   const location = useLocation();
@@ -10,13 +12,17 @@ const RouteLogger = () => {
   useEffect(() => {
     const body = document.querySelector('body');
 
+    body.className = '';
+
     switch(location.pathname){
 
         case '/login':
         case '/signup':
+            body.classList.add('login-page');
             login();
             break;
         case '/home':
+            body.classList.add('home-page');
             home();
             break;
         default:
